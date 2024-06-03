be4ede797b926736c2aa1d2d223317e736be1c82
imKira97 <codingshiva97>
2024-06-03 21:18:31 +0530

    till delete

--- models/product.js
+++ models/product.js
@@ -1,21 +1,80 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   //here we save our product
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp; //this will operation which we will perform either insert or update
+    /* if this.id is set it means we are updating  */
+    if (this._id) {
+      //update
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this }); //it will set other property
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+
+    return dbOp
       .then((res) => {
         console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  //getALl product
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  //getById
+  //here we will use next to get next document also here it will be last  document
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  //delete
+  static deleteProduct(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((product) => {
+        console.log(product);
+        return product;
       })
       .catch((err) => {
         console.log(err);
